4ff7fc67e30521f10daa748bf4cde826981d712d
bt <c1c4c5c6@gmail.com>
2019-04-24 12:42:38 +0800

    psf_normalization

--- PSF.py
+++ PSF.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft,ifft
+from scipy.fftpack import fft, ifft
 
 def normalize(d):
     # d is a (n x dimension) np array
@@ -9,8 +9,7 @@ def normalize(d):
     d /= np.ptp(d, axis=0)
     return d
 
-#xprofile_DPMBead
-path = r"E:\DPM\20190420_mag\test.txt"
+path = r"/home/bt/文件/bosi_optics/DPM_verify/bead_xprofile.txt"
 # os.chdir(path)
 
 x = []
@@ -24,18 +23,28 @@ x = np.array(x).astype(float)
 y = np.array(y).astype(float)
 
 # centralize
-x = x-126
+x = x-139
 # moving average
-y = np.convolve(y, np.ones((18,))/18, mode='same')
-for i in range(len(x)):
-    if (abs(x[i])>50):
-        y[i] = 0
+moving_window = 1  # no moving window
+y = np.convolve(y, np.ones((moving_window,))/moving_window, mode='same')
+# background 0
+# for i in range(len(x)):
+#     if (abs(x[i])>47):
+#         y[i] = 0
+
+# hight adjust
+buf = []
+for i, j in zip(x, y):
+    if (i<-75) or (i>75):
+        buf.append(j)
+base = np.mean(buf)
+y = y - base
 
 n = 1.598
 n0 = 1.566
 beadradius_at_ccd = 4.8*46.5/5.5  # 42.27 pixel
 
-# set
+# compute ideal bead
 x_ideal = np.arange(x[0], x[len(x)-1]+1)
 h = []
 for i in x_ideal:
@@ -46,16 +55,6 @@ for i in x_ideal:
 h = np.array(h)
 # h (pixel)
 y_ideal = 2*np.pi/532*(h*5.5/46.5*1000)*(n-n0)
-
-#plot
-plt.figure(1, dpi=300)
-plt.plot(x, y, label="measurement")
-plt.plot(x_ideal, y_ideal, label="ideal")
-plt.legend()
-plt.title("raw profile of bead")
-plt.xlabel("x(pixel)")
-plt.ylabel("phase")
-plt.show()
 
 # measured FFT
 yf = fft(y)    # 取絕對值
@@ -72,6 +71,31 @@ xf_ideal = np.arange(len(y_ideal))  # 頻率
 xf1_ideal = xf_ideal
 xf2_ideal = xf1_ideal[range(int(len(x_ideal)/2))] #取一半區間
 
+# over in F domain
+over = yf/yf_ideal
+# inverse FT
+y_i = ifft(over)
+# normalize
+y_ii = normalize(y_i)
+
+buf = []
+for i,j in zip(x,y_ii):
+    if (i>-20) and (i<0):
+        if (abs(j-0.5)<=0.2):
+            um = i * 5.5 /46.5
+            buf.append(um)
+            print(um)
+
+#plot
+plt.figure(1, dpi=300)
+plt.plot(x, y, label="measurement")
+plt.plot(x_ideal, y_ideal, label="ideal")
+plt.legend()
+plt.title("raw profile of bead")
+plt.xlabel("x(pixel)")
+plt.ylabel("phase")
+plt.show()
+
 plt.figure(2, dpi=250)
 plt.plot(xf2, yf2, label="measurement")
 plt.plot(xf2_ideal, yf2_ideal, label="ideal")
@@ -81,15 +105,12 @@ plt.xlabel("freq")
 plt.ylabel("au")
 plt.show()
 
-over = yf/yf_ideal
-y_i = ifft(over)
-y_ii = normalize(y_i)
-
 plt.figure(3, dpi=250)
-plt.plot(x, y_ii, label="PSF")
+plt.plot(x*5.5/46.5, y_ii, label="PSF")
 plt.legend()
 plt.title("PSF")
-plt.xlabel("x(pixel)")
+plt.xlabel("x(um)")
+plt.xticks(np.arange(min(x), max(x)+1, 1))
 plt.ylabel("au")
 plt.xlim(-5,5)
 plt.show()
