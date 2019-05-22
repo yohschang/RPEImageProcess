70c65aa01d15bc12c274a7af8bf7ff9cd31052a6
BT <c1c4c5c6@gmail.com>
2019-05-22 13:25:21 +0800

    bead correlated coef

--- crop_30724096image.py
+++ crop_30724096image.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class BT_image(object):
@@ -15,24 +16,55 @@ class BT_image(object):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         self.img = img
         self.name = path.split("\\")[-1]
+        self.board = None
+        self.flat_board = None
 
-    def Beadcenter2croprange(self, x, y):
+    def beadcenter2croprange(self, x, y):
         return x - 84, y - 84
 
     def crop(self, x_center, y_center):
         w = 168
         h = 168
-        x, y = self.Beadcenter2croprange(x_center, y_center)
+        x, y = self.beadcenter2croprange(x_center, y_center)
         self.img = self.img[y:y+h, x:x+w]
 
-    def write_image(self, path, file_name):
-        cv2.imwrite(path + file_name + ".bmp", self.img)
+    def crop_img2circle(self):
+        """choose the area of bead and append to a list"""
+        radius = 50  # pixel
+        self.board = np.zeros((self.img.shape[0], self.img.shape[0]))
+        self.flat_board = []
+        centerx, centery = self.img.shape[0]//2 - 1, self.img.shape[0]//2 - 1
+        for i in range(self.img.shape[0]):
+            for j in range(self.img.shape[0]):
+                if (i - centerx)**2 + (j - centery)**2 <= 2500:
+                    self.board[i, j] = self.img[i, j]
+                    self.flat_board.append(self.img[i, j])
+
+    def write_image(self, path):
+        cv2.imwrite(path + self.name.split(".")[0] + "_circle.bmp", self.img)
 
     def plot_it(self):
         plt.figure()
         plt.title(self.name.split(".")[0])
         plt.imshow(self.img, plt.cm.gray)
         plt.show()
+
+    def plot_board(self):
+        plt.figure()
+        plt.title(self.name.split(".")[0])
+        plt.imshow(self.board, plt.cm.gray)
+        plt.show()
+
+    def normalize_after_crop(self):
+        background = round(float(np.mean(self.img[:20, :20])), 2)
+        self.img = self.img - background
+
+
+def round_all_the_entries_ndarray(matrix, decimal):
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            matrix[i, j] = round(matrix[i, j], decimal)
+    return matrix
 
 
 def stack(path, file_name):
@@ -49,36 +81,45 @@ def stack(path, file_name):
 
 
 path = "E:\\DPM\\20190521\\8position\\"
-list = glob.glob(path + "*_save.bmp")
+list_ = glob.glob(path + "*_save.bmp")
 
 # manually find the center of bead
 center_list = [[265, 2792],
                [2814, 2848],
-               [1543, 2820],
+               [1543, 2822],
                [1579, 1548],
                [301, 1507],
-               [2859, 1566],
-               [323, 229],
-               [2900, 285],
+               [2862, 1564],
+               [326, 226],
+               [2901, 285],
                [1618, 262]]
 
-im = []
-for i in range(len(list)):
-    img_tem = BT_image(list[i])
+im_square = []
+im_circle = []
+for i in range(len(list_)):
+    img_tem = BT_image(list_[i])
     img_tem.crop(center_list[i][0], center_list[i][1])
-    img_tem.plot_it()
-    im.append(img_tem.img.flatten())
+    img_tem.normalize_after_crop()
+    # img_tem.plot_it()
+    # img_tem.write_image(path)
+    im_square.append(img_tem.img.flatten())
 
-matrix = np.corrcoef(im)
+    # circle
+    img_tem.crop_img2circle()
+    img_tem.plot_board()
+    im_circle.append(img_tem.flat_board)
 
+# Input is an array
+matrix_square = np.corrcoef(im_square)
+matrix_circle = np.corrcoef(im_circle)
+matrix_circle = round_all_the_entries_ndarray(matrix_circle, 3)
+col = ["bottom left", "bottom right", "bottom",
+       "center", "left", "right",
+       "top left", "top right", "top"]
 
-
-# img1 = BT_image(list[0])
-# img2 = BT_image(list[2])
-# img1.crop(265, 2792)
-# img2.crop(1543, 2820)
-# img1.plot_it()
-# img2.plot_it()
+# dataframe visualization
+df = pd.DataFrame(data=matrix_circle, columns=col)
+df.insert(0, " ", col)
 
 
 
